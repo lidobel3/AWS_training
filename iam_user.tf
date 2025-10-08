12f5cfe231d91bcd76f8c5f168b8757d9ea56a79
lidobel3 <lidobel3@gmail.com>
2025-10-08 03:10:33 +0200

    iam policy update

--- iam_user.tf
+++ iam_user.tf
@@ -6,6 +6,16 @@ resource "aws_iam_user" "lidobel" {
   name = "lidobel"
 }
 
+resource "aws_iam_user_login_profile" "lidobel_login" {
+  user    = aws_iam_user.lidobel.name
+  password = "MotDePasseSecurise123!"  # Remplace par un mot de passe sécurisé
+  password_reset_required = true  # L'utilisateur devra changer son mot de passe lors de la première connexion
+}
+
+resource "aws_iam_access_key" "lidobel_access_key" {
+  user = aws_iam_user.lidobel.name
+}
+
 resource "aws_iam_policy" "lidobel_policy" {
   name        = "lidobel-policy"
   description = "Politique d'accès limité pour l'utilisateur lidobel"
@@ -21,6 +31,24 @@ resource "aws_iam_policy" "lidobel_policy" {
         Action   = "s3:GetObject"
         Effect   = "Allow"
         Resource = "arn:aws:s3:::mon_bucket/*"
+      },
+      {
+        Action = [ 
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:CreateCluster",
+          "eks:DeleteCluster",
+          "eks:UpdateClusterConfig",
+          "eks:UpdateClusterVersion",
+          "eks:DescribeNodegroup",
+          "eks:ListNodegroups",
+          "eks:CreateNodegroup",
+          "eks:DeleteNodegroup",
+          "eks:UpdateNodegroupConfig",
+          "eks:UpdateNodegroupVersion"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
