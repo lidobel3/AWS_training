ab77b810b3232bb7098f631855b952dcf57dd129
lidobel3 <lidobel3@gmail.com>
2025-11-16 17:56:35 +0100

    new ports open

--- main.tf
+++ main.tf
@@ -62,6 +62,19 @@ resource "aws_security_group" "ssh_sg" {
     cidr_blocks = ["0.0.0.0/0"] # à restreindre à ton IP publique en prod
   }
 
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # à restreindre à ton IP publique en prod
+  }
+
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # à restreindre à ton IP publique en prod
+  }
   egress {
     from_port   = 0
     to_port     = 0
