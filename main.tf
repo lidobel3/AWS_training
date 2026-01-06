cfec6fd85541630f675c8b10cd436595c00d24f7
lidobel3 <lidobel3@gmail.com>
2026-01-06 19:02:09 +0100

    resizing root_block device

--- main.tf
+++ main.tf
@@ -74,6 +74,11 @@ resource "aws_instance" "mon_ec2" {
   key_name               = aws_key_pair.ma_cle.key_name
   vpc_security_group_ids = [aws_security_group.ssh_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  
+  root_block_device {
+  volume_size = 30      # nouvelle taille souhaitée en Go
+  volume_type = "gp3"   # type de volume performant
+  }
 
   user_data = <<-EOF
               #!/bin/bash
