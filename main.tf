3da6e9bd54c20b56b732e4f23bbe82b08ade598a
lidobel3 <lidobel3@gmail.com>
2026-01-07 12:59:16 +0100

    Fermeture port Jenkins

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ resource "aws_security_group" "ssh_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # à restreindre à ton IP publique en prod
   }
-    ingress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # à restreindre à ton IP publique en prod
-  }
+  #   ingress {
+  #   from_port   = 8081
+  #   to_port     = 8081
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"] # à restreindre à ton IP publique en prod
+  # }
     ingress {
     from_port   = 9100
     to_port     = 9100
